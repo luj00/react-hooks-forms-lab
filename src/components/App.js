4691934b2bec17dfb2ccfe6d93f3572bac83f01d
root <root@DESKTOP-598GE48.localdomain>
2023-12-19 13:35:56 +0300

    Assignment completed

--- src/components/App.js
+++ src/components/App.js
@@ -13,7 +13,10 @@ function App() {
 
   return (
     <div className={"App " + (isDarkMode ? "dark" : "light")}>
-      <Header isDarkMode={isDarkMode} onDarkModeClick={handleDarkModeClick} />
+      <Header
+        isDarkMode={isDarkMode}
+        onDarkModeClick={handleDarkModeClick}
+      />
       <ShoppingList items={items} />
     </div>
   );
--- src/components/Filter.js
+++ src/components/Filter.js
@@ -1,10 +1,30 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
-function Filter({ onCategoryChange }) {
+function Filter({ search, onCategoryChange, onSearchChange }) {
+  const [searchText, setSearchText] = useState(search);
+
+  useEffect(() => {
+    setSearchText(search);
+  }, [search]);
+
+  const handleSearchChange = (e) => {
+    const text = e.target.value;
+    setSearchText(text);
+    onSearchChange(text);
+  };
+
   return (
     <div className="Filter">
-      <input type="text" name="search" placeholder="Search..." />
-      <select name="filter" onChange={onCategoryChange}>
+      <input
+        type="text"
+        name="search"
+        placeholder="Search..."
+        value={searchText}
+        onChange={handleSearchChange}
+      />
+      <select
+        name="filter"
+        onChange={onCategoryChange}>
         <option value="All">Filter by category</option>
         <option value="Produce">Produce</option>
         <option value="Dairy">Dairy</option>
